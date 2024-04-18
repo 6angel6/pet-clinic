d442d7954850c97b419e5c0cfb32982aefff5b7f
r_angel <143842574+6angel6@users.noreply.github.com>
2024-04-18 23:53:43 +0500

    UserEntity

---
+++ src/main/java/com/angel/petclinic/model/User.java
@@ -0,0 +1,73 @@
+package com.angel.petclinic.model;
+
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"username"})
+@Table(name = "user", schema = "public")
+public class User {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "sequence_user"
+    )
+    @SequenceGenerator(
+            name = "sequence-user",
+            sequenceName = "sequence_user",
+            allocationSize = 5
+    )
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 50, nullable = false)
+    private String firstName;
+
+    @Column(length = 50, nullable = false)
+    private String lastName;
+
+    @Column(length = 50, nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<Pet> pets = new HashSet<>();
+
+    public void addPet(Pet pet){
+        pets.add(pet);
+        pet.setUser(this);
+    }
+
+    public void removePet(Pet pet){
+        pets.remove(pet);
+        pet.setUser(this);
+    }
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Roles> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        roles.add(role);
+        role.getUsers().add(this);
+    }
+
+    public void removeRole(Role role) {
+        roles.remove(role);
+        role.getUsers().remove(this);
+    }
+
+}
